4073834cec36664be4e394b28adf4ec43c4a9c1d
Precious Kosisochukwu <PreciousKosisochukwu01@gmail.com>
2022-01-26 20:51:37 +0100

    Fix prepareQuestion method

--- src/services/user-assessment.service.js
+++ src/services/user-assessment.service.js
@@ -5,21 +5,40 @@ const CustomError = require("../utils/custom-error");
 
 class UserAssessmentService{
     async prepareAssessment(data){
+
         const {userId, assessmentId} = data;
+
+        let assessmentQuestionCollection = [];
         let response = {};
+
         // get assessment
         const assessment = await AssessmentService.getOne(assessmentId);
 
         if(assessment){
             const questions = await QuestionService.fetchQuestions(assessment.questionBank[0]._id,assessment.numberOfQuestion);
+            
             // Check if user assessment already exist
             const existingAssessment = await UserAssessment.findOne({user: userId, assessment: assessmentId});
-            if(!existingAssessment){         
-                
+            if(!existingAssessment){    
+
+                // Loop through each question to create a child collection for the parent
+                for(let question of questions){
+
+                    const userAssessmentCollection = {
+                        question : question,
+                        choice: "",
+                        isCorrect: false,
+                        markGotten: 0,
+                    }
+
+                    assessmentQuestionCollection.push(userAssessmentCollection)
+                }
+
+                // user assessment parent collection
                 const userAssessment = {
                     user : userId,
                     assessment: assessmentId,
-                    assessmentScript: questions,
+                    assessmentScript: assessmentQuestionCollection,
                     mark : 0
                 }
                 
@@ -27,7 +46,7 @@ class UserAssessmentService{
                 response = await new UserAssessment(userAssessment).save();
             }
             response =  await UserAssessment.findOne({user: userId, assessment: assessmentId})
-            .populate("user assessment assessmentScript.question","-__v -createdAt -updatedAt");
+            .populate("user assessment assessmentScript.question", "-__v -createdAt -updatedAt -password");
         }
 
         return response;
